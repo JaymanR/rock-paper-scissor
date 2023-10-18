2ab1a44c656445f580fd722619a6e1ab7e08bf1e
JaymanR <jayman.code@gmail.com>
2023-10-18 00:59:13 -0400

    write function playRound()

--- myscript.js
+++ myscript.js
@@ -1,15 +1,46 @@
 
 function getComputerChoice() {
+
     const random = Math.floor(Math.random()*3) + 1;
     console.log(random);
 
     switch(random) {
         case 1:
-            return `Rock`;
+            return `rock`;
         case 2:
-            return `Paper`;
+            return `paper`;
         case 3:
-            return `Scissor`;
-    }
-    
+            return `scissor`;
+    }   
 }
+
+function playRound(playerSelection, computerSelection) {
+    
+    switch(playerSelection.toLowerCase()) {
+        case `rock`:
+            if (computerSelection === `rock`) {
+                return `Stalemate`;
+            } else if (computerSelection === `paper`) {
+                return `You Lose! Paper beats Rock`;
+            } else {
+                return `You Win! Rock beats Scissor`;
+            }
+        case `paper`:
+            if (computerSelection === `paper`) {
+                return `Stalemate`;
+            } else if (computerSelection === `scissor`) {
+                return `You Lose! Scissor beats Paper`;
+            } else {
+                return `You Win! Paper beats Rock`;
+            }
+        case `scissor`:
+            if (computerSelection === `scissor`) {
+                return `Stalemate`;
+            } else if (computerSelection === `rock`) {
+                return `You Lose! Rock beats Scissor`;
+            } else {
+                return `You Win! Scissor beats Paper`;
+            }
+    }
+}
+
